4d9aaf2eea484bb2a4d14a630d52f7b6a22f49d9
abedmus <160133466+abedmus@users.noreply.github.com>
2024-08-20 23:08:12 +0300

    Update TestLight.cs

--- Assets/Scripts/TestLight.cs
+++ Assets/Scripts/TestLight.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class TestLight : MonoBehaviour
-{
-    void Start()
-    {
-        GetComponent<Light2D>().intensity = 100;
-    }
-    
+{   
     void Update()
     {
         GetComponent<Light2D>().intensity = Mathf.PingPong(Time.time, 1f);
